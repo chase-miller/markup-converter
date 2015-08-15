d6273dfdc097974c2a321be2df5b95c20b1a1008
Chase Miller <chase.amiller@gmail.com>
2015-08-15 11:36:02 -0400

    Initial Commit

---
+++ MarkupConverter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarkupConverterWeb.Startup))]
+namespace MarkupConverterWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
