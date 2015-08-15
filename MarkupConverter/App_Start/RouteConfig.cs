04ce08d58f773bdc9dc144edffd667f45b9f0a50
Chase Miller <chase.amiller@gmail.com>
2015-08-15 12:33:48 -0400

    Added MarkupConverter controller and view

--- MarkupConverter/App_Start/RouteConfig.cs
+++ MarkupConverter/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MarkupConverterWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MarkupConverter", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MarkupConverter/Controllers/MarkupConverterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarkupConverterWeb.Controllers
+{
+    public class MarkupConverterController : Controller
+    {
+        // GET: MarkupConverter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
